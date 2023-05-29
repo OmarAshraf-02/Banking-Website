3a343dab62a863e0c79b1bd90f13ee55404b5dd7
aly raafat <alewaraafat@gmail.com>
2023-05-29 17:13:31 +0300

    adjust back button

--- milestone3/banking-website/src/shared/components/BackButton.js
+++ milestone3/banking-website/src/shared/components/BackButton.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { IconButton } from "@mui/material";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function BackButton({ to }) {
+    const location = useLocation();
+    const currentPath = location.pathname.split("/");
+    console.log(currentPath)
     return (
-        <Link to={`/client${to}`}>
+        <Link to={`/${currentPath[1]}${to}`}>
             <IconButton>
                 <ArrowBackIosIcon />
             </IconButton>
