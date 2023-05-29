fbccfa75f278996cc1f6d9e4e320e799692cc7ea
aly raafat <alewaraafat@gmail.com>
2023-05-29 23:57:52 +0300

    empty

--- milestone3/banking-website/src/client/pages/Transfers/InternalTransfer.js
+++ milestone3/banking-website/src/client/pages/Transfers/InternalTransfer.js
@@ -213,10 +213,10 @@ const InternalTransfer = () => {
                             ) : (
                                 <>
                                     {isConfirmed && <CheckIcon style={{ marginRight: '10px', color: 'green' }} />}
-                                    {/* <Button type="submit" color="secondary" variant="contained">
+                                    <Button type="submit" color="secondary" variant="contained">
                                         Confirm
-                                    </Button> */}
-                                    <ResponseDialog submit='CONFIRM' response='success'/>
+                                    </Button>
+                                    {/* <ResponseDialog submit='CONFIRM' response='success'/> */}
                                 </>
                             )}
                         </Box>
